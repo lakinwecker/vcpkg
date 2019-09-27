9a18d8b909e1991eac142ce970e52f6428b89345
Lakin Wecker <lakin.wecker@ucalgary.ca>
2019-09-27 11:12:24 -0600

    Speed up the gdal build by using all available cores for make.

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -338,6 +338,9 @@ elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STRE
       message(FATAL_ERROR "MAKE not found")
   endif()
 
+  include(ProcessorCount)
+  ProcessorCount(NUM_CPUS)
+
   if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
     ################
     # Release build
@@ -353,7 +356,7 @@ elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STRE
 
     message(STATUS "Building ${TARGET_TRIPLET}-rel")
     vcpkg_execute_required_process(
-      COMMAND make
+      COMMAND make "-j${NUM_CPUS}"
       WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
       LOGNAME make-build-${TARGET_TRIPLET}-release
     )
